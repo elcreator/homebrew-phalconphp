5a45491ed1ce806ac6325859b40877f5d16fe273
Artur Kyryliuk <mail@artur.work>
2021-05-23 16:00:26 +0300

    Update abstract-php-extension.rb

--- Abstract/abstract-php-extension.rb
+++ Abstract/abstract-php-extension.rb
@@ -239,3 +239,21 @@ class AbstractPhp72Extension < AbstractPhpExtension
     depends_on "php72" => opts if build.with?("homebrew-php")
   end
 end
+
+class AbstractPhp73Extension < AbstractPhpExtension
+  include AbstractPhpVersion::Php73Defs
+
+  def self.init(opts = [])
+    super()
+    depends_on "php73" => opts if build.with?("homebrew-php")
+  end
+end
+
+class AbstractPhp74Extension < AbstractPhpExtension
+  include AbstractPhpVersion::Php74Defs
+
+  def self.init(opts = [])
+    super()
+    depends_on "php74" => opts if build.with?("homebrew-php")
+  end
+end
